81db2f67586a3e6473a4e79013e33967d25664df
Jose Aureo <jose.sleite@pagar.me>
2024-04-18 20:33:19 -0300

    fix: file path correction

--- application/application.js
+++ application/application.js
@@ -2,4 +2,4 @@ const logMessage = require('./helpers/logger.js')
 
 logMessage('application.log', 'User logged in', 'INFO')
 logMessage('application.log', 'Failed login attempt', 'WARNING')
-logMessage('/teste2/eapplication.log', 'Server Error', 'ERROR')
+logMessage('application.log', 'Server Error', 'ERROR')
