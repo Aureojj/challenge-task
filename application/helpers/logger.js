55b335439bab9a412e69db1f707e92fbf24b4811
Jose Aureo <jose.sleite@pagar.me>
2024-04-18 19:59:05 -0300

    feat: create an fuction to generate a log file

--- application/helpers/logger.js
+++ application/helpers/logger.js
@@ -1,17 +1,24 @@
 const fs = require('fs')
 const moment = require('moment')
 
-function logMessage (filename, message, level) {
+function logMessage (filename, message, type) {
   const timestamp = moment().format('YYYY-MM-DD HH:mm:ss')
-  const createMessage = `[${timestamp}] [${level}] ${message}\n`
+  const createMessage = `[${timestamp}] [${type}] ${message}\n`
 
-  fs.appendFile(filename, createMessage, (err) => {
-    if (err) {
-      console.error('Error creating log file:', err)
-    }
+  if (typeof filename !== 'string' || typeof message !== 'string' || typeof type !== 'string') {
+    throw new Error('All arguments must be strings')
+  }
+
+  return new Promise((resolve, reject) => {
+    fs.appendFile(filename, createMessage, (err) => {
+      if (err) {
+        console.error('Error creating log file:', err)
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
   })
-
-  console.log(createMessage)
 }
 
 module.exports = logMessage
