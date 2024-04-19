3a350ac8db969c51d72f81345647e8b579180355
Jose Aureo <jose.sleite@pagar.me>
2024-04-19 01:09:50 -0300

    refactor: refactory products-test file

---
+++ tests/unit-tests/helpers/products-data.js
@@ -0,0 +1,7 @@
+const productsData = [
+  { name: 'Product A', price: 100, stock: 5 },
+  { name: 'Product C', price: 50, stock: 10 },
+  { name: 'Product B', price: 200, stock: 3 }
+]
+
+module.exports = productsData
--- tests/unit-tests/products-list.test.js
+++ tests/unit-tests/products-list.test.js
@@ -1,88 +1,59 @@
 const sortProducts = require('../../inventory-management/sort-products.js')
+const productsData = require('../unit-tests/helpers/products-data.js')
 
 describe('Sort products', () => {
   it('should sort products by price in ascending order', () => {
-    const products = [
-      { name: 'Product A', price: 100, stock: 5 },
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product C', price: 50, stock: 10 }
-    ]
     const expected = [
       { name: 'Product C', price: 50, stock: 10 },
       { name: 'Product A', price: 100, stock: 5 },
       { name: 'Product B', price: 200, stock: 3 }
     ]
-    expect(sortProducts(products, 'price', true)).toEqual(expected)
+    expect(sortProducts(productsData, 'price', true)).toEqual(expected)
   })
 
   it('should sort products by price in descending order', () => {
-    const products = [
-      { name: 'Product A', price: 100, stock: 5 },
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product C', price: 50, stock: 10 }
-    ]
     const expected = [
       { name: 'Product B', price: 200, stock: 3 },
       { name: 'Product A', price: 100, stock: 5 },
       { name: 'Product C', price: 50, stock: 10 }
     ]
-    expect(sortProducts(products, 'price', false)).toEqual(expected)
+    expect(sortProducts(productsData, 'price', false)).toEqual(expected)
   })
 
   it('should sort products by name in ascending order', () => {
-    const products = [
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product A', price: 100, stock: 5 },
-      { name: 'Product C', price: 50, stock: 10 }
-    ]
     const expected = [
       { name: 'Product A', price: 100, stock: 5 },
       { name: 'Product B', price: 200, stock: 3 },
       { name: 'Product C', price: 50, stock: 10 }
     ]
-    expect(sortProducts(products, 'name', true)).toEqual(expected)
+    expect(sortProducts(productsData, 'name', true)).toEqual(expected)
   })
 
   it('should sort products by name in descending order', () => {
-    const products = [
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product A', price: 100, stock: 5 },
-      { name: 'Product C', price: 50, stock: 10 }
-    ]
     const expected = [
       { name: 'Product C', price: 50, stock: 10 },
       { name: 'Product B', price: 200, stock: 3 },
       { name: 'Product A', price: 100, stock: 5 }
     ]
-    expect(sortProducts(products, 'name', false)).toEqual(expected)
+    expect(sortProducts(productsData, 'name', false)).toEqual(expected)
   })
 
   it('should sort products by stock in ascending order', () => {
-    const products = [
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product C', price: 50, stock: 10 },
-      { name: 'Product A', price: 100, stock: 5 }
-    ]
     const expected = [
       { name: 'Product B', price: 200, stock: 3 },
       { name: 'Product A', price: 100, stock: 5 },
       { name: 'Product C', price: 50, stock: 10 }
     ]
-    expect(sortProducts(products, 'stock', true)).toEqual(expected)
+    expect(sortProducts(productsData, 'stock', true)).toEqual(expected)
   })
 
   it('should sort products by stock in descending order', () => {
-    const products = [
-      { name: 'Product B', price: 200, stock: 3 },
-      { name: 'Product C', price: 50, stock: 10 },
-      { name: 'Product A', price: 100, stock: 5 }
-    ]
     const expected = [
       { name: 'Product C', price: 50, stock: 10 },
       { name: 'Product A', price: 100, stock: 5 },
       { name: 'Product B', price: 200, stock: 3 }
     ]
-    expect(sortProducts(products, 'stock', false)).toEqual(expected)
+    expect(sortProducts(productsData, 'stock', false)).toEqual(expected)
   })
 
   it('should throw an error if products is not an array', () => {
@@ -92,24 +63,10 @@ describe('Sort products', () => {
   })
 
   it('should throw an error if sortKey is not a string or number', () => {
-    const products = [
-      { name: 'Product A', price: 100 },
-      { name: 'Product B', price: 50 },
-      { name: 'Product C', price: 150 }
-    ]
-    expect(() => {
-      sortProducts(products, true, true)
-    }).toThrow()
+    expect(() => { sortProducts(productsData, true, true) }).toThrow()
   })
 
   it('should throw an error if ascending is not a boolean', () => {
-    const products = [
-      { name: 'Product A', price: 100 },
-      { name: 'Product B', price: 50 },
-      { name: 'Product C', price: 150 }
-    ]
-    expect(() => {
-      sortProducts(products, 'price', 'not a boolean')
-    }).toThrow()
+    expect(() => { sortProducts(productsData, 'price', 'not a boolean') }).toThrow()
   })
 })
